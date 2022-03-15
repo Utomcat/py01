05e8f2878d5581f0dd81a057b41a49ed396162e2
ranyk <1390851149@qq.com>
2022-03-15 15:57:50 +0800

    feat(py01;): 增加 print 函数测试;
    1. 创建 print01.py 文件,增加 print 测试方法;

---
+++ print01.py
@@ -0,0 +1,34 @@
+# CLASS_NAME: print01.py <br/>
+# Description:  print 函数使用<br/>
+# @author ranyk <br/>
+# @version V1.0 <br/>
+# @date 2022 - 03 - 14
+
+# 1. 输出字符串
+# print("aaaa")
+
+# 2. 输出多个字符串
+# print("aaaa", "bbb")
+
+# 3. 输出整型数据
+# print(1)
+
+# 4. 输出浮点型数据
+# print(2.2)
+
+# 5. 输出布尔型数据
+# print(False)
+
+# 6. 输出两种数据类型
+# print(1, "aaa")
+
+# 7. 输出多个值,且多个值之间使用指定的分隔符分割
+# print("aaaa", 1111, sep="_")
+
+# 8. 输出多个值,且最后不用回车符作为结束标志
+# print("aaaa", 444, end="")
+
+# 9. 输出到文件
+# fp = open("./aa.txt", "a+")
+# print("hello", "world", file=fp, flush=False)
+# fp.close()
