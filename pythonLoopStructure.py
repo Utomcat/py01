0e7e0be91044fe5e6c2ec2813e6d09d3b682bfd6
ranyk <1390851149@qq.com>
2023-02-07 17:28:25 +0800

    feat(py01;): 增加逻辑结构测试类型

--- pythonLoopStructure.py
+++ pythonLoopStructure.py
@@ -22,6 +22,15 @@ print("1~100 的总和为: ", s)
 # print("结束")
 
 # while 循环使用 else
+"""
+执行输出结果: 
+0  小于 5
+1  小于 5
+2  小于 5
+3  小于 5
+4  小于 5
+5  大于或等于 5
+"""
 count = 0
 while count < 5:
     print(count, " 小于 5")
@@ -29,3 +38,88 @@ while count < 5:
 else:
     print(count, " 大于或等于 5")
 
+# while 简单语句组
+# flag = 1
+# while flag: print("输出flag: ", flag)
+# print("程序结束")
+
+# for 遍历列表
+"""
+执行输出结果: 
+Baidu
+Google
+Runoob
+Taobao
+"""
+sites = ["Baidu", "Google", "Runoob", "Taobao"]
+for site in sites:
+    print(site)
+
+# for 遍历字符串
+"""
+执行输出结果: 
+letter ==>  r
+letter ==>  u
+letter ==>  n
+letter ==>  o
+letter ==>  o
+letter ==>  b
+"""
+word = 'runoob'
+for letter in word:
+    print("letter ==> ", letter)
+
+# for...else 输出 0~6 结束后输出 Finally finished
+'''
+输出结果:
+0
+1
+2
+3
+4
+5
+Finally finished!
+'''
+for x in range(6):
+    print(x)
+else:
+    print("Finally finished!")
+
+# for...else 中间出现终止循环 break
+'''
+输出结果: 
+循环数据 Baidu
+循环数据 Google
+菜鸟教程!
+完成循环!
+'''
+sites = ["Baidu", "Google", "Runoob", "Taobao"]
+for site in sites:
+    if site == "Runoob":
+        print("菜鸟教程!")
+        break
+    print("循环数据 " + site)
+else:
+    print("没有循环数据!")
+print("完成循环!")
+
+# 在字母为 o 时 执行 pass 语句块
+'''
+执行结果: 
+当前字母 : R
+当前字母 : u
+当前字母 : n
+执行 pass 块
+当前字母 : o
+执行 pass 块
+当前字母 : o
+当前字母 : b
+Good bye!
+'''
+for letter in 'Runoob':
+    if letter == 'o':
+        pass
+        print('执行 pass 块')
+    print('当前字母 :', letter)
+
+print("Good bye!")
